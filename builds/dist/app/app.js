d4b2a8efc4fe5ed59187fbeafd747612e4c7d64f
Anykey <anykey84@gmail.com>
2016-09-07 12:02:55 +0400

    Добавил анимацию прокрутки до блока

--- builds/dist/app/app.js
+++ builds/dist/app/app.js
@@ -1,15 +1,34 @@
 $(function() {
-  console.log('start');
+    console.log('start');
 
-  // var videobackground = new $.backgroundVideo($('#top'), {
-  //     "align": "centerXY",
-  //     "width": '100%',
-  //     "path": "video/",
-  //     "filename": "LightStreaksDeepBlue",
-  //     "types": ["mp4"],
-  //     "preload": true,
-  //     "autoplay": true,
-  //     "loop": true
-  //   });
+    // var videobackground = new $.backgroundVideo($('#top'), {
+    //     "align": "centerXY",
+    //     "width": '100%',
+    //     "path": "video/",
+    //     "filename": "LightStreaksDeepBlue",
+    //     "types": ["mp4"],
+    //     "preload": true,
+    //     "autoplay": true,
+    //     "loop": true
+    //   });
+
+    $('#integrated-solutions a').on('click', function() {
+      scrollTo($('#magic'));
+    })
+
+    function scrollTo(element) {
+        console.log("!");
+        var offset = 0;
+        // скорость скролла
+        var speed = 1000;
+
+        // место скролла
+        var top = $(element).offset().top + offset;
+
+        $('html, body').animate({
+            scrollTop: top
+        }, speed);
+    }
+
 
 })
